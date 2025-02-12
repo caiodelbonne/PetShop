97b3ce0851f721054bf582f0a5ddb5b186f74c91
Caio Cesar <caiocesar_01@hotmail.com>
2025-02-12 01:29:42 -0300

    bugs final

--- src/services/novoAgendamento.js
+++ src/services/novoAgendamento.js
@@ -1,13 +1,13 @@
 import { apiConfig } from "./api-config.js";
 
-export async function novoAgendamento({ id, nome, pet, data, descricao, observacao }) {
+export async function novoAgendamento({  nome, pet, data, descricao, observacao }) {
     try {
       const response =  await fetch(`${apiConfig.baseURL}/agendamentos`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ id, nome, pet, data, descricao, observacao }),  // Incluindo o nome do pet
+            body: JSON.stringify({  nome, pet, data, descricao, observacao }),  // Incluindo o nome do pet
         });
         if (!response.ok) {
             throw new Error(`Erro ao agendar: ${response.statusText}`);
